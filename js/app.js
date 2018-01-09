8946237fdc2aa6fc4a1247aa2391c7b32eee5bd0
Bruce Seymour <brucedseymour@gmail.com>
2018-01-09 14:59:35 -0500

    version 1.0

--- js/app.js
+++ js/app.js
@@ -1,141 +1,135 @@
-
-const cards =
-[
-"fa-anchor",
-"fa-bicycle",
-"fa-bolt",
-"fa-bomb",
-"fa-cube",
-"fa-diamond",
-"fa-leaf",
-"fa-paper-plane-o",
-"fa-anchor",
-"fa-bicycle",
-"fa-bolt",
-"fa-bomb",
-"fa-cube",
-"fa-diamond",
-"fa-leaf",
-"fa-paper-plane-o"
+const cards = [
+  "fa-anchor",
+  "fa-bicycle",
+  "fa-bolt",
+  "fa-bomb",
+  "fa-cube",
+  "fa-diamond",
+  "fa-leaf",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bicycle",
+  "fa-bolt",
+  "fa-bomb",
+  "fa-cube",
+  "fa-diamond",
+  "fa-leaf",
+  "fa-paper-plane-o"
 ]
 
 const deck = $('.deck');
 const cardCount = 16;
-var clickedCards = [];
-var openCards = [];
-var moveCount = 0;
-
+let clickedCards = [];
+let openCards = [];
+let moveCount = 0;
+let totalMatches = 0;
 
 shuffle(cards);
 
-for (let i=0; i<cardCount; i++){
+// assign cards to grid
+for (let i = 0; i < cardCount; i++) {
   deck.append(`
     <li class="card" id=${i}>
       <i class="fa ${cards[i]}"></i>
    </li>`);
 }
 
-
-
-function showCard(clickedCard){
-    $(clickedCard).attr("class", "card open show");
+// open card
+function showCard(clickedCard) {
+  $(clickedCard).attr("class", "card open show");
 }
 
-function updateScore(){
+// update score
+function updateScore() {
   moveCount++;
   $("#score").text(moveCount);
 }
 
-function openCard(card){
-    card.attr("class", "card open show");
+// open card
+function openCard(card) {
+  card.attr("class", "card open show");
 }
 
-function closeCard(card){
-  card.removeClass("open show");
-  openCards = [];
+// close cards
+function closeCards(card1, card2){
+  setTimeout(function() {
+  $(card1).removeClass("open show");
+  }, 300);
+
+  setTimeout(function() {
+    $(card2).removeClass("open show");
+    openCards.splice(0, 2);
+  }, 600);
 }
 
-function closeCard2(card){
-  card.removeClass("open show");
-  openCards = [];
-  console.log('here2');
-}
-
-
-$(".restart").on('click', function(){
+// reset
+$(".restart").on('click', function() {
   window.location.reload();
 });
 
-
 // User clicks card
+$('li').on('click', function() {
 
-$('li').on('click' , function(){
-
-  updateScore();
   openCard($(this));
+  updateScore();
 
   let clickedCardName = $(this).find('i');
+      clickedCardName = clickedCardName.attr('class');
   let clickedCardId = "#" + $(this).attr('id');
- console.log("Card Name: ", clickedCardName);
-// clickedCardId = "#0";
 
-  clickedCardName = clickedCardName.attr('class');
+  var cardInfo = {
+    cardId: "",
+    cardName: ""
+  }
 
+  cardInfo.cardId = clickedCardId;
+  cardInfo.cardName = clickedCardName;
+  openCards.push(cardInfo);
 
-if( $.inArray(clickedCardName, openCards)!="-1"  ){
+  // keep first card open
+  if (openCards.length > 1) {
+
+  // detect match on second click, but don't compare if user clicked the same card twice
+    if (openCards[0].cardName == openCards[1].cardName && openCards[0].cardId != openCards[1].cardId) {
+      openCards.splice(0, 2); // remove cards from the open cards array
+      totalMatches++;
+    }
+  // if selections are not a match
+    else {
+
+      var cardInfo = {
+        cardId: "",
+        cardName: ""
+      }
+      cardInfo.cardId = clickedCardId;
+      cardInfo.cardName = clickedCardName;
+      closeCards(openCards[0].cardId, openCards[1].cardId);
+    }
+  }
+
+  if (totalMatches == 8) {
     openCard($(this));
-  console.log("Match!!");
-}else {
-console.log("ID: ", clickedCardId);
-    openCards.push(clickedCardName);
-    setTimeout(function(){ $(clickedCardId).removeClass("open show"); }, 400);
-
-
-  console.log("NO Match!!");
-}
+    setTimeout(function() {
+      alert(`Great Work! You won in ${moveCount} moves.`);
+    },100);
+  }
 
 });
 
 
-
-// <li class="card">
-// <li class="card match">
-// <li class="card open show">
-
-
-
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
